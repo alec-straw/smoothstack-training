4e13d0042063a8a9cb5341bc84455a769527957d
alec-straw <90656648+alec-straw@users.noreply.github.com>
2021-09-15 12:55:08 -0500

    Create AppendText.java

---
+++ Java Training/src/com/ss/jb3/assignment2/AppendText.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ss.jb3.assignment2;
+
+/**
+ * @author alecs
+ *
+ */
+public class AppendText {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
